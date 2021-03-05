4d55c3ad4173ace598150443cb3e4f585a5be717
jlondonob <jlondonob>
2021-03-05 13:06:13 -0500

    Finca Raiz Scraper goes all pages

--- scrapySpider/scrapySpider/spiders/FincaRaiz.py
+++ scrapySpider/scrapySpider/spiders/FincaRaiz.py
@@ -1,24 +1,34 @@
 
 # Import libraries
 import scrapy
-from urllib.parse import urljoin
+from scrapy.exceptions import CloseSpider
+from scrapy.http import Request
 
-# Scraper body
+# Initial URL.
+# -- Change later to also include apartment sales and sales in other cities
+# -- Fortunately its very easy to do so as website shows infinite pages
+# -- As expl: https://www.fincaraiz.com.co/apartamento-casa/venta/cundinamarca-antioquia-valle-del-cauca-atlantico/?ad=30|1000||||1||8,9|||67,55,82,57|||||||||||||||||1|||1||griddate%20desc||||-1||||
+# -- 8,9 refers to houses and aptmts. 67, 55, ... referst ot departments
+# -- Try deleting all text between .co/ and /? and search will be the same
+URL = 'https://www.fincaraiz.com.co/casas/venta/medellin/?ad=30|{0}||||1||9|||55|5500006|||||||||||||||||||1||griddate%20asc||||||||'
+
+# Body
+# -- Starts at `URL` when {0} == 1. Checks if there is a next page button
+# -- If next page button exists it goes to next page else the spider closes.
 class FincaraizSpider(scrapy.Spider):
-    
-    # Naming the spider
     name = 'FincaRaiz'
-    
-    # Starting URL
-    start_urls = ['https://www.fincaraiz.com.co/casas/venta/medellin/?ad=30|1||||1||9|||55|5500006|||||||||||||||||||1||griddate%20asc||||||||']
-    
-    # Scrape urls from first page
-    def parse(self,response):
-        base_url = 'https://www.fincaraiz.com.co/'
-        urls = response.xpath("//a[contains(@href,'casa-en-venta')]/@href").getall()
+    start_urls = [URL.format(1)]  
 
-        clean_urls = [urljoin(base_url, url) for url in urls]
-        
-        yield {
-            'urls': clean_urls
-        }
+    def __init__(self):
+        self.page_number = 1
+    
+    def parse(self,response):
+        print(self.page_number)
+        print("----------")
+
+        next_page = response.xpath('//a[@title="Ir a la pagina Siguiente"]')
+        if not next_page:
+            raise CloseSpider("No more pages")
+
+        self.page_number += 1
+        yield Request(URL.format(self.page_number))
