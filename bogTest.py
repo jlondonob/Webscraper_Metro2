06c61421a3fb6bea7a103447c7a707958583dd91
jlondonob <jlondonob>
2021-03-23 17:35:07 -0500

    bogTest.py able to scrape all houses on sale in Bogotá (still pending to scrape apartments).

--- bogTest.py
+++ bogTest.py
@@ -23,6 +23,8 @@ webdriver = webdriver.Chrome(
 
 with webdriver as driver:
 
+    wait = WebDriverWait(driver, 10)
+
     driver.get(url)  
 
     #accept cookies to remove banner
@@ -40,11 +42,40 @@ with webdriver as driver:
     for i in range(0,6):
         min_area.click()
         driver.find_element_by_xpath(areas_button[i]).click()
-
         max_area.click()
         driver.find_element_by_xpath(areas_button[i+1]).click()
-    
-    #delete after checking process
-    time.sleep(10)
+
+        #---
+        #CODE TO GO TRHOUGH PAGES AND SCRAPE
+        #---
+
+        next_page_button = driver.find_element_by_css_selector('.item-icon-next a')
+        arrow_disabled = driver.find_elements_by_css_selector('.item-icon-next.page-item.disabled')
+
+        links1 = driver.find_elements_by_css_selector('.card-result-img .sc-bdVaJa')
+        links1 = [link.get_attribute('href') for link in links1]
+
+        while True:
+            if len(arrow_disabled) > 0:
+                print('No more pages left')
+                break
+            else:
+                # Clicks next page and checks if button is disabled
+                next_page_button.click()
+                arrow_disabled = driver.find_elements_by_css_selector('.item-icon-next.page-item.disabled')
+
+                # Only purpose of this chunk is testing. It may be deleted
+                current_page=driver.find_element_by_css_selector('.page-item.active')
+
+                wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, '.card-result-img .sc-bdVaJa')))
+
+                # Gets links from specific page
+                links2 = driver.find_elements_by_css_selector('.card-result-img .sc-bdVaJa')
+                links2 = [link.get_attribute('href') for link in links2]
+
+                print('Number of urls scraped at page' + current_page.text + ':' + str(len(links2)))
+
+                # Appends new links to links object
+                links1.extend(links2)
 
     driver.close()
